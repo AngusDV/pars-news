04c361ac3990e1c924d28ad1190267daa2ca1de3
hassan mousavi <hsmsender1370@gmail.com>
2024-11-22 18:41:47 +0330

    FIX(docker instal):remove check file exists

--- src/Commands/DockerInstallCommand.php
+++ src/Commands/DockerInstallCommand.php
@@ -39,6 +39,7 @@ class DockerInstallCommand extends Command
     {
         $this->copyFilesIfNotExists();
     }
+
     protected function copyFilesIfNotExists()
     {
         $filesToCopy = [
@@ -51,19 +52,18 @@ class DockerInstallCommand extends Command
         ];
 
         foreach ($filesToCopy as $source => $destination) {
-            // Check if the destination file or directory already exists
-            if (!file_exists($destination)) {
-                // Determine if the source is a directory or file
-                if (is_dir($source)) {
-                    // Create the destination directory
-                    mkdir($destination, 0755, true);
-                    // Copy contents of the directory
-                    $this->copyDirectory($source, $destination);
-                } else {
-                    // Copy the file
-                    copy($source, $destination);
-                }
+
+            // Determine if the source is a directory or file
+            if (is_dir($source)) {
+                // Create the destination directory
+                mkdir($destination, 0755, true);
+                // Copy contents of the directory
+                $this->copyDirectory($source, $destination);
+            } else {
+                // Copy the file
+                copy($source, $destination);
             }
+
         }
     }
 
@@ -86,10 +86,7 @@ class DockerInstallCommand extends Command
                 // Recursively copy the directory
                 $this->copyDirectory($sourcePath, $destinationPath);
             } else {
-                // Copy the file
-                if (!file_exists($destinationPath)) {
-                    copy($sourcePath, $destinationPath);
-                }
+                copy($sourcePath, $destinationPath);
             }
         }
     }
